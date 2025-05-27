f061e29b8b6b93a005dda3295c8ab8d7912c2ac6
LizzethSusana <20213tn012@utez.edu.mx>
2025-05-27 15:51:27 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvlsft9b/Cvlsft9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvlsft9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvlsft9bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cvlsft9bApplication.class, args);
+	}
+
+}
